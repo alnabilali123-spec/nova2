862c06ae7a02ee4858e8d028539421a7e3ebfcf2
alnabilali123-spec <alnabilali123@gmail.com>
2026-08-29 22:12:22 +0300

    Update SettingsScreen.kt

--- app/src/main/java/com/novatube/app/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/novatube/app/ui/screens/settings/SettingsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsState
 import com.novatube.app.NovaTubeApp
 import com.novatube.app.R
 import com.novatube.app.data.prefs.AppPreferences
